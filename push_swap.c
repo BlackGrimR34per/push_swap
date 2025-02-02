d82dbbc8f8ddf13dbadeac45db9ad12c0906fff9
Yohen Sheraun <ysheraun20@yahoo.com>
2025-02-03 00:43:26 +0800

    Added conversion of inputs into numbers. Have to add stack implementation

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 00:47:36 by ysheraun          #+#    #+#             */
-/*   Updated: 2025/02/02 20:02:20 by yosherau         ###   ########.fr       */
+/*   Updated: 2025/02/03 00:41:44 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,25 @@
 
 int	main(int argc, char *argv[])
 {
-	int	index;
-	if (argc <= 1 || (!argv[1][0] && argc == 2))
+	int		index;
+	char	*str;
+
+	index = 0;
+	str = "";
+	if (argc < 2 || (argc == 2 && !argv[1][0]))
 		return (1);
 	else if (argc == 2)
 		argv = ft_split(argv[1], ' ');
-	index = 0;
-	while (argv[index])
-		printf("%s", argv[index++]);
-	return (0);
+	else
+	{
+		while (argv[++index])
+		{
+			str = ft_strjoin(str, argv[index]);
+			str = ft_strjoin(str, " ");
+		}
+		argv = ft_split(str, ' ');
+		index = -1;
+		while (argv[++index])
+			printf("%d\n",ft_atoi(argv[index]));
+	}
 }
